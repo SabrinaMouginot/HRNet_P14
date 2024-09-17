9c864ff9686da92d0d65658df58485686b686767
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-17 16:00:50 +0200

    creation d un formulaire

---
+++ src/components/MyForm.jsx
@@ -0,0 +1,18 @@
+function MyForm() {
+
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input/>
+      </div>
+      <div>
+        <label>Email</label>
+        <input/>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default MyForm;
