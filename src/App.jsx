6c3021af0c454efa0eadbab12b78142b3c09ae84
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-17 15:25:37 +0200

    modification du routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Users from './pages/Users';
+import CreateEmployee from './pages/CreateEmployee';
+import EmployeeList from './pages/EmployeeList';
 import Error from './pages/Error';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<CreateEmployee />} />
+        <Route path="/users" element={<EmployeeList />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
