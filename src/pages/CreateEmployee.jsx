831d5c24cadcced5181cf5b671a83bb7664964ef
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-17 16:01:29 +0200

    Composant MyForm insere dans la page CreateEmployee

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,8 +1,11 @@
+import MyForm from "../components/MyForm";
+
 function CreateEmployee() {
     return (
-      <>
-        
-      </>
+      <div>
+      <h1>Create Employee</h1>
+      <MyForm />
+    </div>
     );
   }
   
