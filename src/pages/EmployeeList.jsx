4ad7c89b406668c70b4b29f55a792c29d71751b5
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-18 17:14:32 +0200

    travail sur le tableau de EmployeeList

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,8 +1,26 @@
 function EmployeeList() {
     return (
-      <>
-        
-      </>
+      <div className="container">
+      <h1>Current Employees</h1>
+
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Start Date</th>
+              <th>Department</th>
+              <th>Date of Birth</th>
+              <th>Street</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip Code</th>
+            </tr>
+          </thead>
+        </table>
+      
+      <a href="/">Home</a>
+    </div>
     );
   }
   
