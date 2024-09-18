770653f15a460e00212233e3dd44d2259a7402ee
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-18 16:50:31 +0200

    Creation de la page d erreur

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,10 @@
 function Error() {
     return (
-      <>
-        
-      </>
+      <div className="container">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
+      <a href="/">Go back to Home</a>
+    </div>
     );
   }
   
