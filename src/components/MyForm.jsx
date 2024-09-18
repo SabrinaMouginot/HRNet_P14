3c336d9056ed27ea168a83250b6280390272b74c
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-18 17:12:27 +0200

    lien de l ancrage de View Current Employees vers la page EmployeeList

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -2,7 +2,7 @@ function MyForm() {
 
     return (
         <div className='container'>
-            <a href="employee-list.html">View Current Employees</a>
+            <a href="/users">View Current Employees</a>
             <h2>Create Employee</h2>
             <form>
                 <label>First Name</label>
