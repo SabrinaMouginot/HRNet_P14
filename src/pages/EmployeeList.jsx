993d5c89963fa5c773b886aa5f6cf4059c1e850e
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-19 20:42:14 +0200

    EmployeeList -  useEffect

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,8 +1,21 @@
-function EmployeeList() {
-    return (
-      <div className="container">
-      <h1>Current Employees</h1>
+import { useEffect, useState } from "react";
 
+function EmployeeList() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const storedEmployees = JSON.parse(localStorage.getItem("employees"));
+    if (storedEmployees) {
+      setEmployees(storedEmployees);
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Current Employees</h1>
+      {employees.length === 0 ? (
+        <p>No employees found</p>
+      ) : (
         <table>
           <thead>
             <tr>
@@ -17,11 +30,26 @@ function EmployeeList() {
               <th>Zip Code</th>
             </tr>
           </thead>
+          <tbody>
+            {employees.map((employee, index) => (
+              <tr key={index}>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>{employee.startDate}</td>
+                <td>{employee.department}</td>
+                <td>{employee.dateOfBirth}</td>
+                <td>{employee.street}</td>
+                <td>{employee.city}</td>
+                <td>{employee.state}</td>
+                <td>{employee.zipCode}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      
+      )}
       <a href="/">Home</a>
     </div>
-    );
-  }
-  
-  export default EmployeeList;
+  );
+}
+
+export default EmployeeList;
