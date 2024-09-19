acc2759ab5c82cfb69a6c85f4c26506575627097
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-19 12:46:32 +0200

    valider les données que l utilisateur va soumettre dans le formulaire

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,3 +1,18 @@
+import { z } from "zod";
+
+// Schéma de validation avec Zod
+const schema = z.object({
+  firstName: z.string().min(1, { message: "First Name is required" }),
+  lastName: z.string().min(1, { message: "Last Name is required" }),
+  dateOfBirth: z.string().min(1, { message: "Date of Birth is required" }),
+  startDate: z.string().min(1, { message: "Start Date is required" }),
+  street: z.string().min(1, { message: "Street is required" }),
+  city: z.string().min(1, { message: "City is required" }),
+  state: z.string().min(1, { message: "State is required" }),
+  zipCode: z.string().min(5, { message: "Zip Code must be at least 5 digits" }),
+  department: z.string().min(1, { message: "Department is required" }),
+});
+
 function MyForm() {
 
     return (
