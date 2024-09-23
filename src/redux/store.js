3929ed67e7f56d475e5dace016e1cc1cb1c1b298
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-24 01:21:31 +0200

    ajout du reducer de la modal dans le store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import employeesReducer from './employeesSlice';
+import modalReducer from './modalSlice';
 
 export const store = configureStore({
   reducer: {
     employees: employeesReducer,
+    modal: modalReducer,
   },
 });
