33a59a07b185078e3b2e32ea0370ec409afb7d76
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-24 14:28:27 +0200

    travail sur la modal _ toggle

--- src/redux/modalSlice.js
+++ src/redux/modalSlice.js
@@ -8,14 +8,11 @@ const modalSlice = createSlice({
   name: 'modal',
   initialState,
   reducers: {
-    openModal: (state) => {
-      state.isOpen = true;
-    },
-    closeModal: (state) => {
-      state.isOpen = false;
+    toggleModal: (state) => {
+      state.isOpen = !state.isOpen;
     },
   },
 });
 
-export const { openModal, closeModal } = modalSlice.actions;
+export const { toggleModal } = modalSlice.actions;
 export default modalSlice.reducer;
