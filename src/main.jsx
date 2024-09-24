58a6dcf4c9d08b7ba6b1c1e15d6f990566cda760
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-24 14:26:14 +0200

    LocalizationProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App.jsx';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </LocalizationProvider>
   </StrictMode>
 );
